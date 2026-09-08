5c11efb1bc8ad8896cb5ce0d8b5cab340f406d61
LindtAna <anastasiialindt@gmail.com>
2026-09-08 18:56:02 +0200

    chore: Initiales Frontend-Setup React + TypeScript mit Vite, Tailwind CSS v3 und shadcn/ui

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+export default function App() {
+  return (
+    <main>
+      <div className="p-8">
+        <Button>Peng</Button>
+      </div>
+    </main>
+
+  )
+}
